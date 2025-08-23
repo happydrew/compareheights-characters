87cab5dba00bf33da311bc781a1d5f28b66b27c5
zhugechina <zhugetd@126.com>
2025-08-23 22:03:00 +0800

    add buildings

--- temp/insert_anime.sql
+++ temp/insert_anime.sql
@@ -2,4 +2,4 @@
 -- 基于权威数据和文件，使用GitHub仓库URL
 
 INSERT INTO public.characters (id, name, height, cat_ids, media_type, media_url, thumbnail_url, color, color_customizable, color_property, order_num) VALUES
-('anime-colossal_titan', 'Colossal Titan', 60, ARRAY[0,3,320], 'image', 'https://raw.githubusercontent.com/happydrew/compareheights-characters/refs/heads/main/anime/attack_on_titan/colossal_titan-60.png', 'https://raw.githubusercontent.com/happydrew/compareheights-characters/refs/heads/main/anime/attack_on_titan/colossal_titan-60.png', NULL, false, NULL, 3000);
+;
