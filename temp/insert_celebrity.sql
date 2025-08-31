7ed53d553d2e5126b35326bf431e40840aa3ab10
zhugechina <zhugetd@126.com>
2025-08-31 14:01:56 +0800

    add some generic human characters

--- temp/insert_celebrity.sql
+++ temp/insert_celebrity.sql
@@ -2,4 +2,4 @@
 -- 基于权威数据和文件，使用GitHub仓库URL
 
 INSERT INTO public.characters (id, name, height, cat_ids, media_type, media_url, thumbnail_url, color, color_customizable, color_property, order_num) VALUES
-('celebrity-nikki_rodriguez', 'Nikki Rodriguez', 1.63, ARRAY[0,2,210], 'image', 'https://raw.githubusercontent.com/happydrew/compareheights-characters/refs/heads/main/celebrity/entertainment_celebs/nikki_rodriguez-1.63.png', 'https://raw.githubusercontent.com/happydrew/compareheights-characters/refs/heads/main/celebrity/entertainment_celebs/nikki_rodriguez-1.63.png', NULL, false, NULL, 2000);
+;
